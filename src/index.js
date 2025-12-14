e2709cbc36769983d17ef796bbd399dd89bea646
Anonim <kadal.rigel@gmail.com>
2025-12-14 13:29:10 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/main.css';
+import './styles/animations.css';
+import './styles/responsive.css';
+import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
